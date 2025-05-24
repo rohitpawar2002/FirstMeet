94fc6189a704b1db9b4eec3a0ca582e179d612df
Omkar Thorat <ankit13868@gmail.com>
2025-05-25 02:11:21 +0530

    Constructor and getter/setter

--- FirstMeet/src/main/java/com/firstmeet/model/MOM.java
+++ FirstMeet/src/main/java/com/firstmeet/model/MOM.java
@@ -21,4 +21,68 @@ public class MOM {
     @ManyToOne
     @JoinColumn(name = "meetingId")
     private MeetingScheduler meeting;
+
+	public MOM() {
+		super();
+	}
+
+	public MOM(Long id, String topic, String action, String owner, String duration, MeetingScheduler meeting) {
+		super();
+		this.id = id;
+		this.topic = topic;
+		this.action = action;
+		this.owner = owner;
+		this.duration = duration;
+		this.meeting = meeting;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public MeetingScheduler getMeeting() {
+		return meeting;
+	}
+
+	public void setMeeting(MeetingScheduler meeting) {
+		this.meeting = meeting;
+	}
+    
+    
 }
